fc1d40cf5ff02594e45da71381282a5b97334de6
John Doe <chiachen1225@gmail.com>
2017-04-23 22:55:46 -0700

    alpha_blog crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,27 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get 'pages/home', to: 'pages#home'
-  root 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  # root 'pages#home'
+  # get 'pages/about', to: 'pages#about'
+ 
+  resources :articles
+#   This will add the following routes:
+#
+# routes path HTTP verb link controller#action
+#
+# articles index articles GET /articles articles#index
+#
+# new article new_article GET /articles/new articles#new
+#
+# create article POST /articles articles#create
+#
+# edit article edit_article GET /articles/:id articles#edit
+#
+# update article PATCH /articles/:id articles#update
+#
+# show article article GET /articles/:id articles#show
+#
+# delete article DELETE /articles/:id articles#destroy
 
 
 end
